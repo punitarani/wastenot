36ce714867207a20d0007fe2398750b5dec2f5fd
Punit Arani <punitsai36@gmail.com>
2023-03-03 20:20:31 -0500

    Create basic api server

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/echo", methods=["GET"])
+def echo():
+    """
+    Echoes the request body back to the client
+    :return: JSON response
+    """
+    return jsonify({"data": request.data.decode("utf-8")})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
