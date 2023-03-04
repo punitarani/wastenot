29489a60eb86c427b1cfaea7adc53c536dc4f0ec
Punit Arani <punitsai36@gmail.com>
2023-03-03 20:34:11 -0500

    Update echo() to specify return type

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import json
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/echo", methods=["GET"])
-def echo():
+def echo() -> json:
     """
     Echoes the request body back to the client
     :return: JSON response
