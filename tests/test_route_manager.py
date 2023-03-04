af83a361a4a527d403b0dfa294288f75fe6358e6
Punit Arani <punitsai36@gmail.com>
2023-03-04 15:12:43 -0500

    Simplify landmarks by removing clusters

--- tests/test_route_manager.py
+++ tests/test_route_manager.py
@@ -7,13 +7,12 @@ from wastenot.models import Address
 
 if __name__ == "__main__":
     landmarks = {
-        "Central Park Zoo": Address("Central Park Zoo", None, "New York", "NY", 10024),
-        "Empire State Building": Address("350 5th Ave", None, "New York", "NY", 10118),
-        "Times Square": Address("Times Square", None, "New York", "NY", 10036),
-        "Rockefeller Center": Address("Rockefeller Center", None, "New York", "NY", 10111),
-        "Grand Central Terminal": Address("Grand Central Terminal", None, "New York", "NY", 10017),
         "Metropolitan Museum of Art": Address("1000 5th Ave", None, "New York", "NY", 10028),
-        "Brooklyn Bridge": Address("Brooklyn Bridge", None, "New York", "NY", 11201),
+        "Madison Square Garden": Address("4 Pennsylvania Plaza", None, "New York", "NY", 10001),
+        "Empire State Building": Address("350 5th Ave", None, "New York", "NY", 10118),
+        "Grand Central Terminal": Address("Grand Central Terminal", None, "New York", "NY", 10017),
+        "Central Park Zoo": Address("Central Park Zoo", None, "New York", "NY", 10024),
+        "Battery Park": Address("Battery Park", None, "New York", "NY", 10004),
     }
 
     # Build the stops dictionary
