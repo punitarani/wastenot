387c728f03711c48ccd39036dd23fdf828ad26a4
Ashay Changwani <ashaychangwani@gmail.com>
2023-03-05 05:33:13 -0500

    Added front end files

---
+++ waste-not/App.js
@@ -0,0 +1,93 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ChatBot from './src/chatbot'
+import Delivery from './src/deliver';
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('./logo.png')} />
+      <Text style={styles.textTitle}>Welcome to Waste Not!</Text>
+      <Text style={styles.text}>40% of all food in the US is wasted every year, resulting in billions of pounds of discarded food. We can help address this by redirecting some of it to people in need.</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Donate')}>
+        <Text style={styles.buttonText}>Donate Food</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Deliver')}>
+        <Text style={styles.buttonText}>Deliver Food</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Donate() {
+  return (
+    <View style={styles.container}>
+       <ChatBot/>
+    </View>
+  );
+}
+
+function Deliver() {
+  return (
+    <View style={styles.container}>
+       <Delivery/>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Waste Not" component={HomeScreen}/>
+        <Stack.Screen name="Donate" component={Donate} />
+        <Stack.Screen name="Deliver" component={Deliver} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  textTitle: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center', 
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    width: '35%',
+    height: '7%',
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+    marginBottom: 20,
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 17,
+  },
+});
---
+++ waste-not/src/chatbot.js
@@ -0,0 +1,161 @@
+import { useState, useRef } from 'react'
+import { View, Text, StyleSheet , FlatList, TextInput, TouchableOpacity, Alert } from 'react-native'
+
+import React from 'react'
+import axios from 'axios'
+
+const ChatBot = () => {
+    const [data, setData] = useState([{ type: "bot", text: "Hi, if you'd like to save the planet bite-by-bite, please let me know details about the food you'd like to donate!" }]);
+    const api_url = 'http://172.30.7.33:8123/chat'
+    const [textInput, setTextInput] = useState('');
+    const [id, setId] = useState(-1);
+    const flatList = useRef(null);
+
+    if(id === -1)
+        setId(Math.floor(Math.random() * 10000) + 1)
+    const handleSend = async () => {
+        const newData = [...data, { type: "user", text: textInput }];
+        setData(newData);
+        setTextInput("");
+        
+        try {
+          const response = await axios.post(
+            api_url,
+            {
+              query: textInput,
+              id: id,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const text = response.data.prompt;
+          const newBotData = [...newData, { type: "bot", text }];
+          setData(newBotData);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Talk to our AI to save some food!</Text>
+       <FlatList
+        data={data}
+        ref = {flatList}
+        keyExtractor={(_, index) => index.toString()}
+        style={styles.body}
+        onContentSizeChange={()=> {flatList.current.scrollToEnd()}}
+        renderItem={({item}) =>(
+            <View style={[styles.chatMessage, item.type === 'user'? styles.chatMessageUser : styles.chatMessageBot]}>
+                {item.type === 'bot' ? <Text style={[styles.botMessageTitle]}>
+                    {'AI: '}
+                  </Text> : null}
+                <Text style={[styles.message,item.type === 'user'? styles.userMessage : styles.botMessage]}>
+                    {item.text}
+                </Text>
+            </View>
+        )}
+        />
+        <TextInput style={styles.input} value = {textInput}
+            onChangeText = {text=>setTextInput(text)}
+            placeholder= "Ask me anything!"
+        />
+        <TouchableOpacity
+            style = {styles.button}
+            onPress={handleSend}>
+            <Text style={styles.buttonText}> Query </Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+export default ChatBot
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    width: '100%',
+    paddingBottom: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    padding: 10,
+    color: '#333'
+    
+  },
+  chatMessage: {
+    flexDirection: 'row',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  chatMessageUser:{
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  chatMessageBot:{
+    flexDirection: 'row'
+  },
+  botMessageTitle: {
+    color: 'red',
+    fontWeight: 'bold',
+    paddingTop: 10,
+  },
+  body: {
+    backgroundColor: '#f1f1f1',
+    width: '90%',
+    height: '80%',
+    margin: 10,
+    borderRadius: 15,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderWidth: 0.5,
+    borderColor: '#696969'
+  },
+  message:{
+    fontSize: 16,
+    maxWidth: '70%',
+    letterSpacing: 0.65,
+    marginLeft: 5,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#d4d4d4'
+  },
+  userMessage: {
+    textAlign:'right',
+  },
+  botMessage: {
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    width: '90%',
+    height: 60,
+    marginBottom: 10,
+    borderRadius: 10,
+    borderColor: '#d4d4d4',
+    padding: 10
+  },
+  button: {
+    backgroundColor: '#ebebff',
+    width: '30%',
+    padding: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    borderColor: '#d4d4d4',
+    borderWidth: 0.5
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  }
+});
