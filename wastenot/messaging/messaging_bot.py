e2fddd3404437a59c91f01bab5ffc6eab160266a
Punit Arani <punitsai36@gmail.com>
2023-03-04 20:43:50 -0500

    Document

--- wastenot/messaging/messaging_bot.py
+++ wastenot/messaging/messaging_bot.py
@@ -1,28 +1,36 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
-import twilio
+"""
+Messaging service
+"""
+
 from twilio.rest import Client
 
 
-class messaging_bot():
+class MessagingBot:
+    """
+    Messaging bot class
+    """
+
     def __init__(self):
-        self.account_sid = 'AC06326780c339640b4e3c84fc6561a867'
-        self.auth_token = '3509ad9473cbacbaa3b30ad309885967'
+        self.account_sid = "AC06326780c339640b4e3c84fc6561a867"
+        self.auth_token = "3509ad9473cbacbaa3b30ad309885967"
         self.client = Client(self.account_sid, self.auth_token)
 
     def send_message(self, message, phone_number):
-        message = self.client.messages \
-            .create(
-                body=message,
-                from_='+15512965505',
-                to='+1'+phone_number
-            )
+        """
+        Send a message to a phone number
+        :param message: Message to send
+        :param phone_number: Phone number to send to
+        :return: True if message was sent, False otherwise
+        """
+
+        message = self.client.messages.create(
+            body=message, from_="+15512965505", to="+1" + phone_number
+        )
         if message is not None:
             return True
         return False
 
 
-
-if __name__ == '__main__':
-    bot = messaging_bot()
-    bot.send_message('Hello','5519995873')
+if __name__ == "__main__":
+    bot = MessagingBot()
+    bot.send_message("Hello", "5519995873")
